c60a5d012849cc77a72e68b51aa943423b8631bb
Joaquindev92 <a1712802@idat.edu.pe>
2022-08-18 22:56:15 -0500

    ef

---
+++ src/main/java/com/idat/EF_JOAQUINGONZALES_MICROSERVICIOIZZERIA/EfJoaquinGonzalesMicroservicioPizzeriaApplication.java
@@ -0,0 +1,13 @@
+package com.idat.EF_JOAQUINGONZALES_MICROSERVICIOIZZERIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfJoaquinGonzalesMicroservicioPizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfJoaquinGonzalesMicroservicioPizzeriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idat/EF_JOAQUINGONZALES_MICROSERVICIOIZZERIA/EfJoaquinGonzalesMicroservicioPizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.EF_JOAQUINGONZALES_MICROSERVICIOIZZERIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfJoaquinGonzalesMicroservicioPizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
